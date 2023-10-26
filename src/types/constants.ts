d79e5aa3e6f69598c8213e00cd6751a722bbfa86
Roy <roylayindeay0@gmail.com>
2023-10-26 11:01:21 +0100

    updated the starting block

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,7 +1,7 @@
 export const CONTRACT_ADDRESS = '0xbD6C7B0d2f68c2b7805d88388319cfB6EcB50eA9';
 export const POLYGON_RPC = 'https://polygon-rpc.com';
 
-export const DEFAULT_STARTING_BLOCK_NO = 47961367;
+export const DEFAULT_STARTING_BLOCK_NO = 47961368;
 
 export const QUEUE_FAILED_RETRY = 3;
 
